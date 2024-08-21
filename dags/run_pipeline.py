6a0e2675d9a2b341b18c4a044c5fa9d6f5480712
Abdallah0799 <tornadogazal@gmail.com>
2024-08-21 20:26:39 +0200

    Dag refactor

--- dags/run_pipeline.py
+++ dags/run_pipeline.py
@@ -2,61 +2,68 @@ from datetime import datetime, timedelta
 from pathlib import Path
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.operators.empty import EmptyOperator
 
 from dbt_airflow.core.config import DbtAirflowConfig, DbtProjectConfig, DbtProfileConfig
 from dbt_airflow.core.task_group import DbtTaskGroup
 from dbt_airflow.core.task import ExtraTask
 from dbt_airflow.operators.execution import ExecutionOperator
-from dbt_airflow.operators.bash import DbtRunBashOperator
 
-from data_engineering.actions import get_pipeline_task_group
+from data_engineering.airflow_utils.extract_load_task_group import (
+    get_task_group_from_source,
+)
 
 import settings
 
 # Import all connectors from source we want to extract
 from data_engineering.api_connectors.mongodb import MongoDbConnector
 from data_engineering.api_connectors.s3 import S3Connector
+from data_engineering.api_connectors.balldontlie import BallDontLie
 
-# Define the required connectors
-mongo_db_database = "center"
-s3_database = "fictive-company"
-
-mongo_db_connector = MongoDbConnector(mongo_db_database)
-s3_connector = S3Connector(s3_database)
 
 # Build the dag
 with DAG(
-    dag_id="test_dag",
+    dag_id="data_pipeline",
     start_date=datetime(2024, 1, 1),
+    schedule="@hourly",
     catchup=False,
-    tags=["example"],
+    tags=["business"],
     default_args={
         "owner": "airflow",
         "retries": 0,
-        "retry_delay": timedelta(minutes=2),
+        "retry_delay": timedelta(minutes=1),
     },
+    dagrun_timeout=timedelta(minutes=50),
 ) as dag:
 
+    # Define the required connectors
+    mongo_db_database = "center"
+    s3_database = "fictive-company"
+    balldontlie_database = "nba"
+
+    mongo_db_connector = MongoDbConnector(mongo_db_database)
+    s3_connector = S3Connector(s3_database)
+    balldontlie_connector = BallDontLie(balldontlie_database)
+
     # Build the required task groups
-    mongodb_to_bq = get_pipeline_task_group(
+    mongodb_to_bq = get_task_group_from_source(
         mongo_db_connector,
         dag,
         {
-            #"filters": {
-            #    "updated_at": {
-            #        "$gte": (datetime.now() - timedelta(settings.MONGO_DB_FETCH_WINDOW))
-            #    }
-            #}
+            "filters": {
+                "updated_at": {
+                    "$gte": (datetime.now() - timedelta(settings.MONGO_DB_FETCH_WINDOW))
+                }
+            }
         },
     )
 
-    s3_to_bq = get_pipeline_task_group(s3_connector,
-                                       dag,
-                                       {
-                                           "datetime_window_filter": settings.S3_FETCH_WINDOW
-                                       })
+    s3_to_bq = get_task_group_from_source(
+        s3_connector, dag, {"datetime_window_filter": settings.S3_FETCH_WINDOW}
+    )
+
+    bdl_to_bq = get_task_group_from_source(
+        balldontlie_connector, dag, {"params": {"per_page": 100}}
+    )
 
     dbt_run = DbtTaskGroup(
         group_id="dbt-bigquery-warehouse",
@@ -73,4 +80,4 @@ with DAG(
         ),
     )
 
-    [mongodb_to_bq, s3_to_bq] >> dbt_run
+    mongodb_to_bq >> s3_to_bq >> bdl_to_bq >> dbt_run
